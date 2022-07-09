8f4d2aabe68e6df1b6e3d77cee26348bb13e7fe1
name <zunmun@gmail.com>
2022-07-09 21:49:11 +0800

    Firebase RemainingBites List

--- src/pages/RemainingBites/index.js
+++ src/pages/RemainingBites/index.js
@@ -1,73 +1,40 @@
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React, {useState, useEffect} from 'react'
-import { Category, Gap, CardNews } from '../../components'
+import { Category, Gap, CardNews, Header, ItemCard } from '../../components'
+import { DefaultTheme, List } from 'react-native-paper'
 import { Firebase } from '../../config'
 
 const RemainingBites = ({navigation}) => {
 
-  const [ category, setCategory] = useState([])
-  const [ mentor, setMentor ] = useState([])
-  const [news, setNews] = useState([])
+  const [ bites, setBites ] = useState([])
   useEffect(() => {
-    Firebase.database().ref('category/').once('value').then(res => {
+    Firebase.database().ref('remainingBites/bites').once('value').then(res => {
       if(res.val()) {
-        setCategory(res.val())
- 
+        setBites(res.val())
       }
     })
-
-    Firebase.database().ref('news/').once('value').then(res => {
-      if(res.val()) {
-        setNews(res.val())
-        console.log(news)
-      }
-    })
-
   }, [])
 
-  const Logout = () => {
-    Firebase.auth().signOut().then(() => {
-      navigation.replace('SplashScreen')
-    }).catch(e => {
-      showMessage({
-        message: e.message,
-        type: 'default',
-        backgroundColor: colors.error,
-        color: colors.white
-      })
-    })
-  }
+
+  const data = bites//["Hello", "1", "2"]
+  
 
   return (
-    <ScrollView style={styles.page}>
-        <Text style={styles.welcome}>
-        Put Food Waste to good use
-      </Text>
-        <View style={styles.wrapperScroll}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <View style={styles.category}>
-            <Gap width={16} />
-            { category.map(item => {
-              return <Category onPress={() => navigation.navigate('ChooseMentor', item)} type={item.category} key={item.id} />
-            })}
-            <Gap width={6} />
-          </View>
-        </ScrollView>
-        <View style={styles.news}>
-            <Text style={styles.welcome}>
-            Announcements and News
-          </Text>
-          <View style={styles.newsContainer}>
-          { news.map((item, index) => {
-              return <CardNews onPress={() => navigation.navigate('News', item)} uri={item.uri} details={item.title} key={index} date={item.date} time={item.time} />
-            })}
-          </View>
-          
-        </View>
-        
-      </View>
-      
-    </ScrollView>
+  <View style={styles.page}>
+    <Header title="Remaining Bites" subtitle="Share and clear any unwanted food" />
+    <ScrollView style={styles.view}>
+      <List.Section >
+             {data.map((detail,index,arr) => {
+                 return (
+                    <View>
+                        <ItemCard  detail={detail.name} additionalInfo={[detail.details]} givenImageURI={detail.imageURL}/>
+                    </View>
+                    
+                 )
+             })}
+      </List.Section>
+      </ScrollView>    
+    </View>
   )
 }
 
@@ -75,31 +42,17 @@ export default RemainingBites
 
 const styles = StyleSheet.create({
     page: {
-        paddingTop: 30,
-        paddingHorizontal: 16,
-        backgroundColor: "white",
-        flex: 1
+        
+        flex: 1,
+        paddingTop: 0,
       },
-      category: {
-        flexDirection: 'row',
-      },
-      wrapperScroll: {
-          marginHorizontal: -16
-      },
-      welcome: {
-        fontSize: 22,
-        marginTop: 30,
-        marginBottom: 15,
-        maxWidth: 300,
-        fontWeight: "600",
-        color: 'black',
-        fontWeight: '600'
-      },
-      news: {
-        paddingHorizontal: 20
-      },
-      newsContainer: {
-        paddingBottom: 100
-      }
+      image: {
+          height: 200,
+          width: '100%',
 
+      },
+      view: {
+        
+        marginTop: 10
+      }
 })
